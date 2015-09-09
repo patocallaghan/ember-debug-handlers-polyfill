9c810f7c35b913be69413c2156c9a08b4248ff4b
Pat O'Callaghan <patocallaghan@gmail.com>
2015-09-09 09:06:05 +0100

    Add support for Ember CLI 0.1.2

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 module.exports = {
   name: 'ember-debug-handlers-polyfill',
 
-  included: function() {
-    var app = this.app;
+  included: function(app) {
+    var app = this.app || app;
 
     if (app.env !== 'production') {
       app.import('vendor/ember-debug-handlers-polyfill/debug.js');
